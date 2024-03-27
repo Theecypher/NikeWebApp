877f36086957aa807d27b4ba2094894011f086de
Theecypher <obioramma12@gmail.com>
2024-03-27 14:55:35 +0100

    added slideshow

--- src/Pages/Spotlight/spotlight.jsx
+++ src/Pages/Spotlight/spotlight.jsx
@@ -1,12 +1,47 @@
+import { useRef, useState } from "react";
 import spotlightData from "./spotlightData";
 
 const Spotlight = () => {
+    const spotRef = useRef(null);
+    const imageRef = useRef(null);
+    const [toggle, setToggle] = useState(false)
+    const [isDown, setIsDown] = useState(false)
+    const [startX, setStartX] = useState(null)
+    const [scrollLeft, setScrollLeft] = useState(0)
+
+    const tabsH = spotRef.current;
+    
+    const handleMouseDown = (e) => {
+      setIsDown(true)
+      setToggle((prev) => !prev)
+      setStartX(e.pageX - tabsH.offsetLeft)
+      setScrollLeft(tabsH.scrollLeft)
+    }
+
+    const handleMouseLeave = (e) => {
+      setIsDown(false)
+      setToggle(false)
+      console.count(true);
+    }
+
+    const handleMouseMove = (e) => {
+      if (!isDown) return;
+      e.preventDefault()
+      const x = e.pageX - tabsH.offsetLeft
+      const walk = (x - startX) * 3
+      tabsH.scrollLeft = scrollLeft - walk
+    }
+
   return (
-    <section className="container ml-[20px]">
+    <section className="container ml-[20px]" ref={spotRef}>
     <h3 className="font-bold text-[18px]">Classics Spotlight</h3>
-    <div className="flex gap-4">
+    <div
+    onMouseLeave={handleMouseLeave}
+    onMouseMove={handleMouseMove}
+    onMouseDown={handleMouseDown}
+     className="flex gap-4">
       {spotlightData.map((item, key) => (
-        <img src={item.img} className="w-[300px] h-[300px] object-contain" alt="" />
+        <img key={key} ref={imageRef} src={item.img} className="w-[300px] h-[300px] object-contain" alt="" />
       ))}
     </div>
   </section>
