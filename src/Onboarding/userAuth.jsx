e11d41038448a68f70fec441f84202c64d556543
Theecypher <obioramma12@gmail.com>
2024-04-26 14:24:56 +0100

    started and designed signup page

--- src/Onboarding/userAuth.jsx
+++ src/Onboarding/userAuth.jsx
@@ -5,7 +5,7 @@ import Button from "../Components/Button";
 const UserAuth = () => {
   return (
     <div className="px-7 flex flex-col gap-3">
-      <div className="flex gap-3 py-2">
+      <div className="flex gap-3 py-5">
         <SiNike className="font-[600] w-10 h-10" />
         <SiJordan className="font-[600] w-10 h-10" />
       </div>
@@ -21,13 +21,14 @@ const UserAuth = () => {
         </div>
 
         <input type="email" className="border-2 border-[#b1aeae] py-3 w-full rounded-xl px-5 my-5" placeholder="Email" />
-        <p className="my-4">
-          By continuning I agree to Nike's
-          <Link className="underline">privacy Policy</Link> and
-          <Link className="underline">Terms of use</Link>
+        <p className="my-4 w-[60%] text-[#7f7e7e]">
+          By continuning, I agree to Nike's
+          <Link className="underline"> privacy Policy</Link> and
+          <Link className="underline"> Terms of use</Link>
         </p>
 
-        <Button text="Continue" blackButton={true} />
+        {/* <Button text="Continue" blackButton={true} /> */}
+        <button className="w-full bg-black px-4 py-3 text-white rounded-3xl">Continue</button>
       </form>
     </div>
   );
