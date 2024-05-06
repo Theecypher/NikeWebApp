261a3724fb48f4c175e1910a4fa1ab8c384a2e33
Theecypher <obioramma12@gmail.com>
2024-05-06 15:09:52 +0100

    started and designed signup, designed the cart component and page

--- src/Components/MightLike/MightData.jsx
+++ src/Components/MightLike/MightData.jsx
@@ -1,26 +1,46 @@
-import img1 from "./Assets/img1.jpeg"
-import img2 from "./Assets/img2.jpeg"
-import img3 from "./Assets/img3.jpeg"
-import img4 from "./Assets/img4.jpeg"
-import img5 from "./Assets/img5.jpeg"
+import img1 from "./Assets/img1.jpeg";
+import img2 from "./Assets/img2.jpeg";
+import img3 from "./Assets/img3.jpeg";
+import img4 from "./Assets/img4.jpeg";
+import img5 from "./Assets/img5.jpeg";
 
 export const MightLikeData = [
-    {
-        img: img1
-    },
-    {
-        img: img2
-    },
-    {
-        img: img3
-    },
-    {
-        img: img4
-    },
-    {
-        img: img5
-    },
-    {
-        img: img1
-    },
-]
+  {
+    img: img5,
+    name: "Nike Air Max 270",
+    description: "Men's Shoes",
+
+    price: "$160",
+  },
+  {
+    img: img4,
+    name: "Nike Court Leg 270",
+    description: "Women's Shoes",
+    price: "$66.9",
+    formerPrice: "$120.00",
+  },
+  {
+    img: img3,
+    name: "Nike Air Max 270",
+    description: "Men;s Shoes",
+    price: "$160",
+  },
+  {
+    img: img1,
+    name: "Nike Air Max 270",
+    description: "Men;s Shoes",
+    price: "$160",
+  },
+  {
+    img: img2,
+    name: "Nike Court Leg 270",
+    description: "Women's Shoes",
+    price: "$69",
+  },
+  {
+    img: img1,
+    name: "Nike Court Leg 270",
+    description: "Women's Shoes",
+    price: "$160",
+  },
+];
--- src/Components/MightLike/MightLink.jsx
+++ src/Components/MightLike/MightLink.jsx
@@ -1,23 +1,63 @@
-
+import { useState } from "react";
 import { MightLikeData } from "./MightData";
 import { RxChevronLeft, RxChevronRight } from "react-icons/rx";
 
 const MightLike = () => {
+  const [buttonState, setButtoneState] = useState({
+    activeObject: null,
+    objects: [
+      { id: 0, img: <RxChevronLeft /> },
+      { id: 1, img: <RxChevronRight /> },
+    ],
+  });
+
+  const toggleActive = (index) => {
+    setButtoneState({
+      ...buttonState,
+      activeObject: buttonState.objects[index],
+    });
+  };
+
+  const toggleActivveStyles = (index) => {
+    if (buttonState.objects[index] === buttonState.activeObject) {
+      return "bg-[#ccc] flex items-center justify-center rounded-full p-2 w-10 h-10 border-2 border-blue-700";
+    } else {
+      return "bg-[#ccc]  flex items-center justify-center rounded-full p-2 w-10 h-10 ";
+    }
+  };
+
   return (
     <div className="px-5 py-4">
       <h1 className="font-semibold text-2xl">You Might Also Like</h1>
 
       <div className="flex gap-2 items-end my-2 justify-end">
-        <RxChevronLeft className="bg-[#ccc] rounded-full p-2 w-10 h-10" />
-        <RxChevronRight className="bg-[#ccc] rounded-full p-2 w-10 h-10" />
-        {/* <RxChevronLeft /> */}
+        {buttonState.objects.map((item, index) => (
+          <div
+          onClick={() => {
+            toggleActive(index)
+          }}
+           key={index} className={toggleActivveStyles(index)}>
+            {item.img}
+          </div>
+        ))}
       </div>
 
-      <div className="flex border gap-3">
+      <div className="flex gap-3">
         {MightLikeData.map((item, i) => (
-          // <div className="" key={i}>
-          <img key={i} className="border shadow-xl w-[250px] border-red-00" src={item.img} alt="" />
-          // </div>
+          <div className="flex-none" key={i}>
+          <img
+            key={i}
+            className="border shadow-md w-[250px] border-red-00"
+            src={item.img}
+            alt=""
+          />
+
+          <div className="my-2">
+            <h3 className="font-semibold">{item.name}</h3>
+            <p className="opacity-40">{item.description}</p>
+            <h5 className="font-semibold my-2 text text-[15px]">{item.price}</h5>
+          </div>
+          </div>
         ))}
       </div>
     </div>
